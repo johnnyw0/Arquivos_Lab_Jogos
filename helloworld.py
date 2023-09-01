81599f3836ef783840ea78a6bd321ef1252ed0b2
johnnyw0 <johnnyawatanabe@gmail.com>
2023-09-01 12:40:31 -0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,8 @@
 from PPlay.window import*
 
-janela = Window(800,500)
+janela = Window(200,200)
 janela.set_title('João André Campos Watanabe')
 janela.delay(3000)
+
+janela.draw_text('Hello World', 0, 0, size=12, color=(0,0,0), font_name='Arial', bold=False, italic=False)
+janela.update()
