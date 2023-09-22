8de3199fe4580057cce37f139fe8549c3723f7c5
johnnyw0 <johnnyawatanabe@gmail.com>
2023-09-22 10:33:53 -0300

    changes

--- pong.py
+++ pong.py
@@ -1,15 +1,27 @@
-import pygame
 from PPlay.window import*
 from PPlay.sprite import*
+from PPlay.gameimage import*
 
 janela = Window(1200, 700)
 janela.set_title("João André Watanabe")
 janela.set_background_color([255,255,255])
 
-
-bola = Sprite("circle-16.png")
+bg = GameImage("png/fundo.png")
+bg.set_position(0, 0)
+bola = Sprite("png/bola.png")
 bola.set_position((janela.height/2)+200, (janela.width/2)-250)
-bola.draw()
+
+velx = vely = 250
 
 while True:
+    bola.move_x(velx*janela.delta_time())
+    bola.move_y(vely*janela.delta_time())
+
+    if bola.x + bola.width >= janela.width or bola.x < 0:
+        velx *= -1
+    if bola.y + bola.height >= janela.height or bola.y < 0:
+        vely *= -1
+
+    bg.draw()
+    bola.draw()
     janela.update()
