f909a78c0f719b68668fda00b98a0470bb840b4e
johnnyw0 <johnnyawatanabe@gmail.com>
2023-09-22 12:49:21 -0300

    changes

--- pong.py
+++ pong.py
@@ -11,15 +11,24 @@ bg.set_position(0, 0)
 bola = Sprite("png/bola.png")
 bola.set_position((janela.height/2)+200, (janela.width/2)-250)
 
-velx = vely = 250
+velx = vely = 500
 
 while True:
     bola.move_x(velx*janela.delta_time())
     bola.move_y(vely*janela.delta_time())
 
-    if bola.x + bola.width >= janela.width or bola.x < 0:
+
+    if bola.x + bola.width >= janela.width:
+        bola.x = janela.width - bola.width
         velx *= -1
-    if bola.y + bola.height >= janela.height or bola.y < 0:
+    if bola.x < 0:
+        bola.x = 0
+        velx *= -1
+    if bola.y + bola.height >= janela.height:
+        bola.y = janela.height - bola.height
+        vely *= -1
+    if bola.y < 0:
+        bola.y = 0
         vely *= -1
 
     bg.draw()
