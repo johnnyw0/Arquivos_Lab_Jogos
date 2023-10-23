d4a0d7f033789560caa8bb2fadecf4239c2328f5
johnnyw0 <johnnyawatanabe@gmail.com>
2023-10-23 11:29:00 -0300

    jhgjk

--- space_invaders/func.py
+++ space_invaders/func.py
@@ -12,7 +12,7 @@ def voltar_menu():
 def atirar(nave, lista):
 
 
-    bala = Sprite("png/bola2.png")
+    bala = Sprite("png/tiro.png")
     bala.x = nave.x + nave.width/2
     bala.y = nave.y - bala.height
 
